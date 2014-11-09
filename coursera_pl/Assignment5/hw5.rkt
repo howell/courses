26589769c72d3a673aa99a755c5c5c9093beb046
Sam Caldwell <samcaldwell@utexas.edu>
2014-11-08 21:11:11 -0600

    hw5

--- coursera_pl/Assignment5/hw5.rkt
+++ coursera_pl/Assignment5/hw5.rkt
@@ -22,7 +22,15 @@
 
 ;; Problem 1
 
-;; CHANGE (put your solutions here)
+(define (racketlist->mupllist xs)
+  (if (null? xs)
+      (aunit)
+      (apair (car xs) (racketlist->mupllist (cdr xs)))))
+
+(define (mupllist->racketlist xs)
+  (if (aunit? xs)
+      null
+      (cons (apair-e1 xs) (mupllist->racketlist (apair-e2 xs)))))
 
 ;; Problem 2
 
@@ -48,7 +56,56 @@
                (int (+ (int-num v1) 
                        (int-num v2)))
                (error "MUPL addition applied to non-number")))]
-        ;; CHANGE add more cases here
+        [(int? e) e]
+        [(aunit? e) e]
+        [(ifgreater? e)
+         (let ([v1 (eval-under-env (ifgreater-e1 e) env)]
+               [v2 (eval-under-env (ifgreater-e2 e) env)])
+           (if (and (int? v1)
+                    (int? v2))
+               (if (> (int-num v1) (int-num v2))
+                   (eval-under-env (ifgreater-e3 e) env)
+                   (eval-under-env (ifgreater-e4 e) env))
+               (error "MUPL ifgreater applied to non-number")))]
+        [(mlet? e)
+         (let* ([v (eval-under-env (mlet-e e) env)]
+                [new-env (cons (cons (mlet-var e) v) env)])
+           (eval-under-env (mlet-body e) new-env))]
+        [(apair? e)
+         (let ([v1 (eval-under-env (apair-e1 e) env)]
+               [v2 (eval-under-env (apair-e2 e) env)])
+           (apair v1 v2))]
+        [(fst? e)
+         (let ([v (eval-under-env (fst-e e) env)])
+           (if (apair? v)
+               (apair-e1 v)
+               (error "MUPL fst applied to non-pair")))]
+        [(snd? e)
+         (let ([v (eval-under-env (snd-e e) env)])
+           (if (apair? v)
+               (apair-e2 v)
+               (error "MUPL snd applied to non-pair")))]
+        [(isaunit? e)
+         (let ([v (eval-under-env (isaunit-e e) env)])
+           (if (aunit? v)
+               (int 1)
+               (int 0)))]
+        [(fun? e) (closure env e)]
+        [(closure? e) e]
+        [(call? e)
+         (let ([v1 (eval-under-env (call-funexp e) env)]
+               [v2 (eval-under-env (call-actual e) env)])
+           (if (closure? v1)
+               (let* ([clos-env (closure-env v1)]
+                      [fun (closure-fun v1)]
+                      [formal (fun-formal fun)]
+                      [arg-env (cons (cons formal v2) clos-env)]
+                      [call-env (if (fun-nameopt fun)
+                                    (cons (cons (fun-nameopt fun) v1) arg-env)
+                                    arg-env)])
+                 (eval-under-env (fun-body fun) call-env))
+               (error "MUPL call applied to non-function")))]
+        
         [#t (error (format "bad MUPL expression: ~v" e))]))
 
 ;; Do NOT change
@@ -57,32 +114,162 @@
         
 ;; Problem 3
 
-(define (ifaunit e1 e2 e3) "CHANGE")
+(define (ifaunit e1 e2 e3)
+  (ifgreater (isaunit e1) (int 0) e2 e3))
 
-(define (mlet* lstlst e2) "CHANGE")
+(define (mlet* lstlst e2)
+  (if (null? lstlst)
+      e2
+      (mlet (car (car lstlst)) (cdr (car lstlst)) (mlet* (cdr lstlst) e2))))
 
-(define (ifeq e1 e2 e3 e4) "CHANGE")
+(define (ifeq e1 e2 e3 e4)
+  (mlet* (list (cons "_x" e1) (cons "_y" e2))
+         (ifgreater (var "_x") (var "_y")
+                    e4
+                    (ifgreater (var "_y") (var "_x")
+                               e4
+                               e3))))
 
 ;; Problem 4
 
-(define mupl-map "CHANGE")
+(define mupl-map
+  (fun "map" "f"
+       (fun "map'" "xs"
+            (ifaunit (var "xs")
+                     (aunit)
+                     (mlet* (list (cons "hd" (fst (var "xs")))
+                                  (cons "tl" (snd (var "xs"))))
+                            (apair (call (var "f") (var "hd")) (call (var "map'") (var "tl"))))))))
 
 (define mupl-mapAddN 
   (mlet "map" mupl-map
-        "CHANGE (notice map is now in MUPL scope)"))
+        (fun #f "i"
+             (call (var "map") (fun #f "x" (add (var "i") (var "x")))))))
 
 ;; Challenge Problem
 
 (struct fun-challenge (nameopt formal body freevars) #:transparent) ;; a recursive(?) 1-argument function
 
+(define (find-free-vars e)
+  (define (search bound e)
+    (cond [(var? e)
+           (let ([s (var-string e)])
+             (if (set-member? bound s) (set) (set s)))]
+          [(int? e) (set)]
+          [(add? e) (set-union (search bound (add-e1 e)) (search bound (add-e2 e)))]
+          [(ifgreater? e) (set-union (search bound (ifgreater-e1 e))
+                                     (search bound (ifgreater-e2 e))
+                                     (search bound (ifgreater-e3 e))
+                                     (search bound (ifgreater-e4 e)))]
+          [(fun? e)
+           (let* ([name-bound (if (fun-nameopt e) (set-add bound (fun-nameopt e)) bound)]
+                  [formal-bound (set-add name-bound (fun-formal e))])
+             (search formal-bound (fun-body e)))]
+          [(call? e) (set-union (search bound (call-funexp e)) (search bound (call-actual e)))]
+          [(mlet? e)
+           (let ([new-bound (set-add bound (mlet-var e))])
+             (set-union (search bound (mlet-e e)) (search new-bound (mlet-body e))))]
+          [(apair? e) (set-union (search bound (apair-e1 e)) (search bound (apair-e2 e)))]
+          [(fst? e) (search bound (fst-e e))]
+          [(snd? e) (search bound (snd-e e))]
+          [(aunit? e) (set)]
+          [(isaunit? e) (search bound (isaunit-e e))]
+          [(closure? e) (set)]
+          [(fun-challenge? e) (fun-challenge-freevars e)]
+          [#t (error (format "bad MUPL expression: ~v" e))]))
+  (search (set) e))
+
 ;; We will test this function directly, so it must do
 ;; as described in the assignment
-(define (compute-free-vars e) "CHANGE")
+(define (compute-free-vars e)
+  (let ([cfv compute-free-vars])
+    (cond [(var? e) e]
+          [(int? e) e]
+          [(add? e) (add (cfv (add-e1 e)) (cfv (add-e2 e)))]
+          [(ifgreater? e) (ifgreater (cfv (ifgreater-e1 e)) (cfv (ifgreater-e2 e)) (cfv (ifgreater-e3 e)) (cfv (ifgreater-e4 e)))]
+          [(fun? e) (fun-challenge (fun-nameopt e) (fun-formal e) (cfv (fun-body e)) (find-free-vars e))]
+          [(call? e) (call (cfv (call-funexp e)) (cfv (call-actual e)))]
+          [(mlet? e) (mlet (mlet-var e) (cfv (mlet-e e)) (cfv (mlet-body e)))]
+          [(apair? e) (apair (cfv (apair-e1 e)) (cfv (apair-e2 e)))]
+          [(fst? e) (fst (cfv (fst-e e)))]
+          [(snd? e) (snd (cfv (snd-e e)))]
+          [(aunit? e) e]
+          [(isaunit? e) (isaunit (cfv (isaunit-e e)))]
+          [(closure? e) e]
+          [(fun-challenge? e) e]
+          [#t (error (format "bad MUPL expression: ~v" e))])))
+        
 
 ;; Do NOT share code with eval-under-env because that will make
 ;; auto-grading and peer assessment more difficult, so
 ;; copy most of your interpreter here and make minor changes
-(define (eval-under-env-c e env) "CHANGE")
+(define (eval-under-env-c e env)
+  (cond [(var? e) 
+         (envlookup env (var-string e))]
+        [(add? e) 
+         (let ([v1 (eval-under-env-c (add-e1 e) env)]
+               [v2 (eval-under-env-c (add-e2 e) env)])
+           (if (and (int? v1)
+                    (int? v2))
+               (int (+ (int-num v1) 
+                       (int-num v2)))
+               (error "MUPL addition applied to non-number")))]
+        [(int? e) e]
+        [(aunit? e) e]
+        [(ifgreater? e)
+         (let ([v1 (eval-under-env-c (ifgreater-e1 e) env)]
+               [v2 (eval-under-env-c (ifgreater-e2 e) env)])
+           (if (and (int? v1)
+                    (int? v2))
+               (if (> (int-num v1) (int-num v2))
+                   (eval-under-env-c (ifgreater-e3 e) env)
+                   (eval-under-env-c (ifgreater-e4 e) env))
+               (error "MUPL ifgreater applied to non-number")))]
+        [(mlet? e)
+         (let* ([v (eval-under-env-c (mlet-e e) env)]
+                [new-env (cons (cons (mlet-var e) v) env)])
+           (eval-under-env-c (mlet-body e) new-env))]
+        [(apair? e)
+         (let ([v1 (eval-under-env-c (apair-e1 e) env)]
+               [v2 (eval-under-env-c (apair-e2 e) env)])
+           (apair v1 v2))]
+        [(fst? e)
+         (let ([v (eval-under-env-c (fst-e e) env)])
+           (if (apair? v)
+               (apair-e1 v)
+               (error "MUPL fst applied to non-pair")))]
+        [(snd? e)
+         (let ([v (eval-under-env-c (snd-e e) env)])
+           (if (apair? v)
+               (apair-e2 v)
+               (error "MUPL snd applied to non-pair")))]
+        [(isaunit? e)
+         (let ([v (eval-under-env-c (isaunit-e e) env)])
+           (if (aunit? v)
+               (int 1)
+               (int 0)))]
+        [(fun? e) (error "fun's should be replaced with fun-challenge")]
+        [(fun-challenge? e)
+         (let ([clos-env (filter (lambda (x)
+                                   (set-member? (fun-challenge-freevars e) (car x)))
+                                 env)])
+           (closure clos-env e))]
+        [(closure? e) e]
+        [(call? e)
+         (let ([v1 (eval-under-env-c (call-funexp e) env)]
+               [v2 (eval-under-env-c (call-actual e) env)])
+           (if (closure? v1)
+               (let* ([clos-env (closure-env v1)]
+                      [fun (closure-fun v1)]
+                      [formal (fun-challenge-formal fun)]
+                      [arg-env (cons (cons formal v2) clos-env)]
+                      [call-env (if (fun-challenge-nameopt fun)
+                                    (cons (cons (fun-challenge-nameopt fun) v1) arg-env)
+                                    arg-env)])
+                 (eval-under-env-c (fun-challenge-body fun) call-env))
+               (error "MUPL call applied to non-function")))]
+        
+        [#t (error (format "bad MUPL expression: ~v" e))]))
 
 ;; Do NOT change this
 (define (eval-exp-c e)
--- coursera_pl/Assignment5/hw5test.rkt
+++ coursera_pl/Assignment5/hw5test.rkt
@@ -5,9 +5,25 @@
 
 ;; Be sure to put your homework file in the same folder as this test file.
 ;; Uncomment the line below and change HOMEWORK_FILE to the name of your homework file.
-;;(require "HOMEWORK_FILE")
+(require "hw5.rkt")
 
 (require rackunit)
+
+(define fib
+  (fun "fib" "n"
+       (ifgreater (int 2) (var "n")
+                  (int 1)
+                  (add (call (var "fib") (add (var "n") (int -1))) (call (var "fib") (add (var "n") (int -2)))))))
+
+(define add-ten
+  (mlet "ten" (int 10)
+        (fun "add-ten" "x"
+             (add (var "x") (var "ten")))))
+
+(define ten
+  (mlet "x" (int 7)
+        (mlet "y" (int 3)
+              (fun "ten" "unused" (add (var "x") (var "y"))))))
 
 (define tests
   (test-suite
@@ -21,35 +37,120 @@
 
    ;; tests if ifgreater returns (int 2)
    (check-equal? (eval-exp (ifgreater (int 3) (int 4) (int 3) (int 2))) (int 2) "ifgreater test")
+   (check-equal? (eval-exp (ifgreater (int 3) (int 3) (int 3) (int 2))) (int 2))
+   (check-equal? (eval-exp (ifgreater (int 3) (int 2) (int 3) (int 2))) (int 3))
+   (check-exn exn:fail? (lambda () (eval-exp (ifgreater (apair (int 1) (int 2)) (int 0) (int 1) (int 2)))))
    
    ;; mlet test
    (check-equal? (eval-exp (mlet "x" (int 1) (add (int 5) (var "x")))) (int 6) "mlet test")
+   (check-exn exn:fail? (lambda () (eval-exp (mlet "x" (int 1) (add (int 5) (var "y"))))))
+   (check-equal? (eval-exp (mlet "x" (int 1) (mlet "x" (int 2) (add (int 5) (var "x"))))) (int 7) "mlet test")
+   (check-equal? (eval-exp (mlet "x" (int 1) (mlet "y" (int 2) (add (var "y") (var "x"))))) (int 3) "mlet test")
    
    ;; call test
    (check-equal? (eval-exp (call (closure '() (fun #f "x" (add (var "x") (int 7)))) (int 1))) (int 8) "call test")
+   (check-equal? (eval-exp (call fib (int 0))) (int 1))
+   (check-equal? (eval-exp (call fib (int 1))) (int 1))
+   (check-equal? (eval-exp (call fib (int 2))) (int 2))
+   (check-equal? (eval-exp (call fib (int 3))) (int 3))
+   (check-equal? (eval-exp (call fib (int 4))) (int 5))
    
    ;;snd test
    (check-equal? (eval-exp (snd (apair (int 1) (int 2)))) (int 2) "snd test")
+   (check-exn exn:fail? (lambda () (eval-exp (snd (int 0)))))
+   
+   ;; fst test
+   (check-equal? (eval-exp (fst (apair (int 1) (int 2)))) (int 1) "fst test")
+   (check-exn exn:fail? (lambda () (eval-exp (fst fib))))
    
    ;; isaunit test
    (check-equal? (eval-exp (isaunit (closure '() (fun #f "x" (aunit))))) (int 0) "isaunit test")
+   (check-equal? (eval-exp (isaunit (aunit))) (int 1) "isaunit test")
    
    ;; ifaunit test
    (check-equal? (eval-exp (ifaunit (int 1) (int 2) (int 3))) (int 3) "ifaunit test")
+   (check-equal? (eval-exp (ifaunit (aunit) (int 2) (int 3))) (int 2) "ifaunit test")
    
    ;; mlet* test
    (check-equal? (eval-exp (mlet* (list (cons "x" (int 10))) (var "x"))) (int 10) "mlet* test")
+   (check-equal? (eval-exp (mlet* (list (cons "x" (int 10)) (cons "y" (add (var "x") (int 1)))) (add (var "x") (var "y")))) (int 21) "mlet* test")
    
    ;; ifeq test
    (check-equal? (eval-exp (ifeq (int 1) (int 2) (int 3) (int 4))) (int 4) "ifeq test")
+   (check-equal? (eval-exp (ifeq (int 2) (int 2) (int 3) (int 4))) (int 3) "ifeq test")
    
    ;; mupl-map test
    (check-equal? (eval-exp (call (call mupl-map (fun #f "x" (add (var "x") (int 7)))) (apair (int 1) (aunit)))) 
                  (apair (int 8) (aunit)) "mupl-map test")
+   (check-equal? (eval-exp (call (call mupl-map (fun #f "x" (add (var "x") (int 7)))) (aunit))) 
+                 (aunit) "mupl-map test")
    
    ;; problems 1, 2, and 4 combined test
    (check-equal? (mupllist->racketlist
    (eval-exp (call (call mupl-mapAddN (int 7))
+                   (racketlist->mupllist 
+                    (list (int 3) (int 4) (int 9)))))) (list (int 10) (int 11) (int 16)) "combined test")
+   
+   ;; challenge problem tests
+   (check-equal? (find-free-vars fib) (set))
+   (check-equal? (find-free-vars (mlet-body add-ten)) (set "ten"))
+   (check-equal? (find-free-vars (mlet-body (mlet-body ten))) (set "x" "y"))
+   (check-equal? (find-free-vars mupl-map) (set))
+   (check-equal? (find-free-vars mupl-mapAddN) (set))
+   
+   ;; tests if ifgreater returns (int 2)
+   (check-equal? (eval-exp-c (ifgreater (int 3) (int 4) (int 3) (int 2))) (int 2) "ifgreater test")
+   (check-equal? (eval-exp-c (ifgreater (int 3) (int 3) (int 3) (int 2))) (int 2))
+   (check-equal? (eval-exp-c (ifgreater (int 3) (int 2) (int 3) (int 2))) (int 3))
+   (check-exn exn:fail? (lambda () (eval-exp-c (ifgreater (apair (int 1) (int 2)) (int 0) (int 1) (int 2)))))
+   
+   ;; mlet test
+   (check-equal? (eval-exp-c (mlet "x" (int 1) (add (int 5) (var "x")))) (int 6) "mlet test")
+   (check-exn exn:fail? (lambda () (eval-exp-c (mlet "x" (int 1) (add (int 5) (var "y"))))))
+   (check-equal? (eval-exp-c (mlet "x" (int 1) (mlet "x" (int 2) (add (int 5) (var "x"))))) (int 7) "mlet test")
+   (check-equal? (eval-exp-c (mlet "x" (int 1) (mlet "y" (int 2) (add (var "y") (var "x"))))) (int 3) "mlet test")
+   
+   ;; call test
+   (check-equal? (eval-exp-c (call (closure '() (fun-challenge #f "x" (add (var "x") (int 7)) (set))) (int 1))) (int 8) "call test")
+   (check-equal? (eval-exp-c (call fib (int 0))) (int 1))
+   (check-equal? (eval-exp-c (call fib (int 1))) (int 1))
+   (check-equal? (eval-exp-c (call fib (int 2))) (int 2))
+   (check-equal? (eval-exp-c (call fib (int 3))) (int 3))
+   (check-equal? (eval-exp-c (call fib (int 4))) (int 5))
+   
+   ;;snd test
+   (check-equal? (eval-exp-c (snd (apair (int 1) (int 2)))) (int 2) "snd test")
+   (check-exn exn:fail? (lambda () (eval-exp-c (snd (int 0)))))
+   
+   ;; fst test
+   (check-equal? (eval-exp-c (fst (apair (int 1) (int 2)))) (int 1) "fst test")
+   (check-exn exn:fail? (lambda () (eval-exp-c (fst fib))))
+   
+   ;; isaunit test
+   (check-equal? (eval-exp-c (isaunit (closure '() (fun #f "x" (aunit))))) (int 0) "isaunit test")
+   (check-equal? (eval-exp-c (isaunit (aunit))) (int 1) "isaunit test")
+   
+   ;; ifaunit test
+   (check-equal? (eval-exp-c (ifaunit (int 1) (int 2) (int 3))) (int 3) "ifaunit test")
+   (check-equal? (eval-exp (ifaunit (aunit) (int 2) (int 3))) (int 2) "ifaunit test")
+   
+   ;; mlet* test
+   (check-equal? (eval-exp-c (mlet* (list (cons "x" (int 10))) (var "x"))) (int 10) "mlet* test")
+   (check-equal? (eval-exp-c (mlet* (list (cons "x" (int 10)) (cons "y" (add (var "x") (int 1)))) (add (var "x") (var "y")))) (int 21) "mlet* test")
+   
+   ;; ifeq test
+   (check-equal? (eval-exp-c (ifeq (int 1) (int 2) (int 3) (int 4))) (int 4) "ifeq test")
+   (check-equal? (eval-exp-c (ifeq (int 2) (int 2) (int 3) (int 4))) (int 3) "ifeq test")
+   
+   ;; mupl-map test
+   (check-equal? (eval-exp-c (call (call mupl-map (fun #f "x" (add (var "x") (int 7)))) (apair (int 1) (aunit)))) 
+                 (apair (int 8) (aunit)) "mupl-map test")
+   (check-equal? (eval-exp-c (call (call mupl-map (fun #f "x" (add (var "x") (int 7)))) (aunit))) 
+                 (aunit) "mupl-map test")
+   
+   ;; problems 1, 2, and 4 combined test
+   (check-equal? (mupllist->racketlist
+   (eval-exp-c (call (call mupl-mapAddN (int 7))
                    (racketlist->mupllist 
                     (list (int 3) (int 4) (int 9)))))) (list (int 10) (int 11) (int 16)) "combined test")
    
