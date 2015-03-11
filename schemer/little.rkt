89d504527fe7312d0f45b6f66f15118ca5b6991c
Sam Caldwell <samcaldwell@utexas.edu>
2015-03-10 22:51:39 -0500

    scheming

---
+++ schemer/little.rkt
@@ -0,0 +1,25 @@
+#lang racket
+
+(define atom?
+  (lambda (x)
+    (and (not (pair? x)) (not (null? x)))))
+
+(define add1
+  (lambda (x)
+    (+ x 1)))
+
+(define sub1
+  (lambda (x)
+    (- x 1)))
+
+(define o+
+  (lambda (n m)
+    (cond
+      ((zero? m) n)
+      (else (add1 (o+ n (sub1 m)))))))
+
+(define o-
+  (lambda (n m)
+    (cond
+      ((zero? m) n)
+      (else (sub1 (o- n (sub1 m)))))))
