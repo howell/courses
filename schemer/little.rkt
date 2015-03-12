765183d3437c81d46f829e84ce3b3009ab810673
Sam Caldwell <samcaldwell@utexas.edu>
2015-03-11 22:41:24 -0500

    more scheming

--- schemer/little.rkt
+++ schemer/little.rkt
@@ -4,6 +4,71 @@
   (lambda (x)
     (and (not (pair? x)) (not (null? x)))))
 
+(define insertR
+  (lambda (new old lat)
+    (cond
+      ((null? lat) (quote ()))
+      (else (cond
+              ((eq? old (car lat)) (cons old (cons new (cdr lat))))
+              (else (cons (car lat) (insertR new old (cdr lat)))))))))
+
+(define insertL
+  (lambda (new old lat)
+    (cond
+      ((null? lat) (quote ()))
+      (else (cond
+              ((eq? old (car lat)) (cons new lat))
+              (else (cons (car lat) (insertL new old (cdr lat)))))))))
+
+(define subst
+  (lambda (new old lat)
+    (cond
+      ((null? lat) (quote ()))
+      (else (cond
+              ((eq? old (car lat)) (cons new (cdr lat)))
+              (else (cons (car lat) (subst new old (cdr lat)))))))))
+
+(define subst2
+  (lambda (new o1 o2 lat)
+    (cond
+      ((null? lat) (quote ()))
+      (else (cond
+              ((eq? o1 (car lat)) (cons new (cdr lat)))
+              ((eq? o2 (car lat)) (cons new (cdr lat)))
+              (else (cons (car lat) (subst2 new o1 o2 (cdr lat)))))))))
+
+(define multirember
+  (lambda (a lat)
+    (cond
+      ((null? lat) lat)
+      (else (cond
+              ((eq? a (car lat)) (multirember a (cdr lat)))
+              (else (cons (car lat) (multirember a (cdr lat)))))))))
+
+(define multiinsertR
+  (lambda (new old lat)
+    (cond
+      ((null? lat) lat)
+      (else (cond
+              ((eq? old (car lat)) (cons old (cons new (multiinsertR new old (cdr lat)))))
+              (else (cons old (multiinsertR new old (cdr lat)))))))))
+
+(define multiinsertL
+  (lambda (new old lat)
+    (cond
+      ((null? lat) lat)
+      (else (cond
+              ((eq? old (car lat)) (cons new (cons old (multiinsertL new old (cdr lat)))))
+              (else (cons old (multiinsertL new old (cdr lat)))))))))
+
+(define multisubst
+  (lambda (new old lat)
+    (cond
+      ((null? lat) lat)
+      (else (cond
+              ((eq? old (car lat)) (cons new (multisubst new old (cdr lat))))
+              (else (cons (car lat) (multisubst new old (cdr lat)))))))))
+
 (define add1
   (lambda (x)
     (+ x 1)))
@@ -23,3 +88,115 @@
     (cond
       ((zero? m) n)
       (else (sub1 (o- n (sub1 m)))))))
+
+(define addtup
+  (lambda (tup)
+    (cond
+      ((null? tup) 0)
+      (else (o+ (car tup) (addtup (cdr tup)))))))
+
+(define o*
+  (lambda (n m)
+    (cond
+      ((zero? m) 0)
+      (else (o+ n (o* n (sub1 m)))))))
+
+(define tup+
+  (lambda (t1 t2)
+    (cond
+      ((and (null? t1) (null? t2)) (quote ()))
+      (else (cons (o+ (car t1) (car t2)) (tup+ (cdr t1) (cdr t2)))))))
+
+(define o>
+  (lambda (n m)
+    (cond
+      ((zero? n) #f)
+      ((zero? m) #t)
+      (else (o> (sub1 n) (sub1 m))))))
+
+(define o<
+  (lambda (n m)
+    (cond
+      ((zero? m) #f)
+      ((zero? n) #t)
+      (else (o< (sub1 n) (sub1 m))))))
+
+(define o=
+  (lambda (n m)
+    (cond
+      ((o< n m) #f)
+      ((o> n m) #f)
+      (else #t))))
+
+(define expt
+  (lambda (n m)
+    (cond
+      ((zero? m) 1)
+      (else (o* n (expt n (sub1 m)))))))
+
+(define o%
+  (lambda (n m)
+    (cond
+      ((o< n m) 0)
+      (else (add1 (o% (o- n m) m))))))
+
+(define length
+  (lambda (lat)
+    (cond
+      ((null? lat) 0)
+      (else (add1 (length (cdr lat)))))))
+
+(define pick
+  (lambda (n lat)
+    (cond
+      ((zero? (sub1 n)) (car lat))
+      (else (pick (sub1 n) (cdr lat))))))
+
+(define rempick
+  (lambda (n lat)
+    (cond
+      ((zero? (sub1 n)) (cdr lat))
+      ((null? lat) (quote ()))
+      (else (cons (car lat) (rempick (sub1 n) (cdr lat)))))))
+
+(define no-nums
+  (lambda (lat)
+    (cond
+      ((null? lat) '())
+      (else (cond
+              ((number? (car lat)) (no-nums (cdr lat)))
+              (else (cons (car lat) (no-nums (cdr lat)))))))))
+
+(define all-nums
+  (lambda (lat)
+    (cond
+      ((null? lat) '())
+      (else (cond
+              ((number? (car lat)) (cons (car lat) (all-nums (cdr lat))))
+              (else (all-nums (cdr lat))))))))
+
+(define eqan?
+  (lambda (a1 a2)
+    (cond
+      ((and (number? a1) (number? a2)) (o= a1 a2))
+      ((or (number? a1) (number? a2)) #f)
+      (else (eq? a1 a2)))))
+
+(define occur
+  (lambda (a lat)
+    (cond
+      ((null? lat) 0)
+      (else (cond
+              ((eq? a (car lat)) (add1 (occur a (cdr lat))))
+              (else (occur a (cdr lat))))))))
+
+(define one?
+  (lambda (n)
+    (zero? (sub1 n))))
+
+(define rempick2
+  (lambda (n lat)
+    (cond
+      ((one? n) (cdr lat))
+      ((null? lat) (quote ()))
+      (else (cons (car lat) (rempick2 (sub1 n) (cdr lat)))))))
