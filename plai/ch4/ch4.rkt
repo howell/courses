8587570b2348f48b7600c3b60f648010e2a35113
Sam Caldwell <sam.caldwell@goketra.com>
2015-04-10 16:34:22 -0500

    plai ch4

---
+++ plai/ch4/ch4.rkt
@@ -0,0 +1,27 @@
+#lang plai-typed
+
+(define-type ArithC
+  [numC (n : number)]
+  [plusC (l : ArithC) (r : ArithC)]
+  [multC (l : ArithC) (r : ArithC)])
+
+(define-type ArithS
+  [numS (n : number)]
+  [plusS (l : ArithS) (r : ArithS)]
+  [multS (l : ArithS) (r : ArithS)]
+  [bminusS (l : ArithS) (r : ArithS)])
+
+(define (desugar [as : ArithS]) : ArithC
+  (type-case ArithS as
+    [numS (n) (numC n)]
+    [plusS (l r) (plusC (desugar l)
+                        (desugar r))]
+    [multS (l r) (multC (desugar l)
+                        (desugar r))]
+    [bminusS (l r) (plusC (desugar l) (multC (numC -1) (desugar r)))]))
+
+#|
+It’s a common mistake to forget the recursive calls to desugar on l and r. What happens when you forget them?
+Try for yourself and see.
+Answer: type error!
+|#
