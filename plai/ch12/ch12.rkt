3d5f1810b2e694c6e3d2675498db503ed1125bd4
Sam Caldwell <samcaldwell@utexas.edu>
2015-04-16 21:26:34 -0500

    plai ch12

---
+++ plai/ch12/ch12.rkt
@@ -0,0 +1,59 @@
+#lang plai-typed
+
+(define-type ExprC
+  [numC (n : number)]
+  [idC (s : symbol)]
+  [appC (fun : ExprC) (arg : ExprC)]
+  [plusC (l : ExprC) (r : ExprC)]
+  [multC (l : ExprC) (r : ExprC)]
+  [lamC (arg : symbol) (body : ExprC)])
+
+(define-type Value
+  [numV (n : number)]
+  [closV (f : (Value -> Value))])
+
+(define-type Binding
+  [bind (name : symbol) (val : Value)])
+
+(define-type-alias Env (listof Binding))
+(define mt-env empty)
+(define extend-env cons)
+
+(define (lookup [x : symbol] [env : Env]) : Value
+  (cond
+    [(empty? env) (error 'lookup "name not found")]
+    [else (cond
+            [(symbol=? x (bind-name (first env))) (bind-val (first env))]
+            [else (lookup x (rest env))])]))
+
+(define (num+ [l : Value] [r : Value]) : Value
+  (cond
+    [(and (numV? l) (numV? r)) (numV (+ (numV-n l) (numV-n r)))]
+    [else (error '+ "expected numbers")]))
+
+(define (num* [l : Value] [r : Value]) : Value
+  (cond
+    [(and (numV? l) (numV? r)) (numV (* (numV-n l) (numV-n r)))]
+    [else (error '* "expected numbers")]))
+ 
+(define (interp [expr : ExprC] [env : Env]) : Value
+  (type-case ExprC expr
+    [numC (n) (numV n)]
+    [idC (n) (lookup n env)]
+    [appC (f a) (local ([define f-value (interp f env)]
+                        [define a-value (interp a env)])
+                  ((closV-f f-value) a-value))]
+    [plusC (l r) (num+ (interp l env) (interp r env))]
+    [multC (l r) (num* (interp l env) (interp r env))]
+    [lamC (a b) (closV (lambda (arg-val)
+                         (interp b
+                                 (extend-env (bind a arg-val)
+                                             env))))]))
+
+#|
+Exercise
+Observe a curious shift. In our previous implementation, the environment was extended in the appC case. Here, it’s extended in the lamC case. Is one of these incorrect?
+If not, why did this change occur?
+Answer
+No, they are both correct. We have to extend the environment in the lamC case now because that is the only place where the identifier a is in scope (in the host language).
+|#
---
+++ plai/ch12/ch12_2.rkt
@@ -0,0 +1,56 @@
+#lang plai-typed
+
+#|
+Exercise
+Convert the above interpreter to use dynamic scope.
+|#
+
+(define-type ExprC
+  [numC (n : number)]
+  [idC (s : symbol)]
+  [appC (fun : ExprC) (arg : ExprC)]
+  [plusC (l : ExprC) (r : ExprC)]
+  [multC (l : ExprC) (r : ExprC)]
+  [lamC (arg : symbol) (body : ExprC)])
+
+(define-type Value
+  [numV (n : number)]
+  [closV (f : (Value Env -> Value))])
+
+(define-type Binding
+  [bind (name : symbol) (val : Value)])
+
+(define-type-alias Env (listof Binding))
+(define mt-env empty)
+(define extend-env cons)
+
+(define (lookup [x : symbol] [env : Env]) : Value
+  (cond
+    [(empty? env) (error 'lookup "name not found")]
+    [else (cond
+            [(symbol=? x (bind-name (first env))) (bind-val (first env))]
+            [else (lookup x (rest env))])]))
+
+(define (num+ [l : Value] [r : Value]) : Value
+  (cond
+    [(and (numV? l) (numV? r)) (numV (+ (numV-n l) (numV-n r)))]
+    [else (error '+ "expected numbers")]))
+
+(define (num* [l : Value] [r : Value]) : Value
+  (cond
+    [(and (numV? l) (numV? r)) (numV (* (numV-n l) (numV-n r)))]
+    [else (error '* "expected numbers")]))
+
+(define (interp [expr : ExprC] [env : Env]) : Value
+  (type-case ExprC expr
+    [numC (n) (numV n)]
+    [idC (n) (lookup n env)]
+    [appC (f a) (local ([define f-value (interp f env)]
+                        [define a-value (interp a env)])
+                  ((closV-f f-value) a-value env))]
+    [plusC (l r) (num+ (interp l env) (interp r env))]
+    [multC (l r) (num* (interp l env) (interp r env))]
+    [lamC (a b) (closV (lambda (arg-val dyn-env)
+                         (interp b
+                                 (extend-env (bind a arg-val)
+                                             dyn-env))))]))
